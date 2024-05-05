073f9046b92d3f8b02dede2c88a7df998465e6b1
Saransh Singhal <singhalsaransh40@gmail.com>
2024-05-05 17:02:04 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Saransh Singhal on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
